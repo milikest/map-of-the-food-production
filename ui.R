b61b9662a812aad26d3c551650a45bda0db6014a
milikest <30753164+milikest@users.noreply.github.com>
2022-07-09 18:22:17 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,32 +8,35 @@
 #
 
 library(shiny)
+url <- "https://github.com/milikest/map-of-the-food-production/raw/main/FAOSTAT_data_7-8-2022-All_Countries.zip"
+download.file(url, destfile = "./data.zip")
+unzip("./data.zip")
 
 data <- read.csv("FAOSTAT_data_7-8-2022-All_Countries.csv")
 
 select_items <- unique(data$Item)
-select_years <- unique(data$Years)
+select_years <- unique(data$Year)
 
-# Define UI for application that draws a histogram
+# Define UI for application that creates a world map
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Food Production Analysis"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("food_var",
-                        label = "Select a Food",
-                        choices = select_items),
-            selectInput("year_var",
-                        label = "Select a Year",
-                        choices = select_years)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Food Production Analysis"),
+  
+  # Sidebar with food and year input
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("food_var",
+                  label = "Select a Food",
+                  choices = select_items),
+      selectInput("year_var",
+                  label = "Select a Year",
+                  choices = select_years)
+    ),
+    
+    # Show a plot of the world map
+    mainPanel(
+      htmlOutput("distPlot")
     )
+  )
 ))
