243a6e4cf134793bbcbd342b81b8543945775ece
milikest <30753164+milikest@users.noreply.github.com>
2022-07-12 16:11:25 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,12 +1,6 @@
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 # Data has been gathered from https://www.fao.org/faostat/en/#data/QCL
-# From the source  All Countries, Production Quantity, Crops Primary>(List) and Crops Processed>List, All years are selected. 
+# From the source  All Countries, Production Quantity, Crops Primary>(List) and Crops Processed>List, 2000-2020 years are selected. 
 library(shiny)
 library(dplyr)
 library(tibble)
@@ -39,8 +33,6 @@ data[is.na(data)] = 0
 colnames(data)[4] <- "Country"
 colnames(data)[12] <- "Tonnes"
 
-
-# Server function
 shinyServer(function(input, output) {
   output$distPlot <- renderGvis({
     gvisGeoChart(filter(data, Year == input$year_var, Item == input$food_var), locationvar = 'Country', colorvar = 'Tonnes',
@@ -54,27 +46,29 @@ shinyServer(function(input, output) {
     plot_ly(x = filter(data, Country==input$country_var, Year==input$year_var2)[c(8,12)][order(filter(data, Country==input$country_var, Year==input$year_var2)[,c(12)], decreasing = T),]$Item[1:10],
             y = filter(data, Country==input$country_var, Year==input$year_var2)[c(8,12)][order(filter(data, Country==input$country_var, Year==input$year_var2)[,c(12)], decreasing = T),][,c(2)][1:10],
             color = ~filter(data, Country==input$country_var, Year==input$year_var2)[c(8,12)][order(filter(data, Country==input$country_var, Year==input$year_var2)[,c(12)], decreasing = T),]$Item[1:10],
-            colors="BrBG",
-            type = "bar") %>% hide_legend()
+            colors="BrBG", type = "bar") %>% hide_legend()
   })
-  
-  df_creater <- reactive({
-  foods <- unique(data$Item)
-  food <- array()
-  rank <- array()
-  for(i in 1:length(foods)){
-    fo_co <- filter(data, Item==foods[i], Year == input$year_var2)[,c(4,12)][order(filter(data,  Item == foods[i], Year == input$year_var2)$Tonnes, decreasing = T),] 
-    if(input$country_var %in% fo_co$Country){
-      rank <- append(rank, which(fo_co$Country == input$country_var))  
-      food <- append(food, foods[i])
-    }
-  }
-  df <- data.frame(food, rank)
-  return(df[order(df$rank,df$food),][1:10,])
+  regression <- reactive({
+    newdf <- data.frame(2000:2020,matrix(sapply(2000:2020,function(i){filter(data, Year==i, Country==input$country_var) %>% summarise(sum(Tonnes))})))
+    colnames(newdf) <- c("index","tonnes")
+    newdf$tonnes <- as.numeric(newdf$tonnes)
+    model <- lm(tonnes~index,newdf)
+    preds <- predict(model)
+    return(preds)
   })
-  
-  output$toprank <-renderDataTable({
-    df_creater()
+  polynomial <- reactive({
+    newdf <- data.frame(2000:2020,matrix(sapply(2000:2020,function(i){filter(data, Year==i, Country==input$country_var) %>% summarise(sum(Tonnes))})))
+    colnames(newdf) <- c("index","tonnes")
+    newdf$tonnes <- as.numeric(newdf$tonnes)
+    model <- lm(tonnes~ poly(index,2,raw=TRUE), newdf)
+    poly_preds <- predict(model)
+    return(poly_preds)
   })
-  
+
+  output$p2 <-renderPlotly({
+    p <- plot_ly(x=2000:2020, y=matrix(sapply(2000:2020,function(i){filter(data, Year==i, Country==input$country_var) %>% summarise(sum(Tonnes))})),
+                 color =2000:2020,colors = c( "#56B1F7","#132B43"), size=22,name="Total Production", mode="markers", type="scatter") %>%
+                add_trace(y=regression(),  name = "Regression Line", line = list(color="purple", width=1), mode="lines") %>%
+                add_trace(y= polynomial(), name = "Polynomial Line (Power=2)", line= list(color="brown", width=1), mode="lines") 
+  })
 })
